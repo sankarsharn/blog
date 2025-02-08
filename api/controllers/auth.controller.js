f587e4d68b15c351f0bd65f4312865812902b474
sankarsharn <sankarsharnrastogi@gmail.com>
2025-02-08 14:10:09 +0530

    User signin route and middleware created

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+import User from "../models/user.model.js"
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/errorHandler.js";
+
+export const signup = async (req , res , next) =>{
+   const { username , email , password } = req.body;
+
+   if(!username || !email || !password || username.trim() === '' || email.trim() === '' || password.trim() === ''){
+       return next(errorHandler('Please fill in all fields' , 400));
+    }
+   
+   const hashedPassword = await bcryptjs.hash(password , 12);
+   const newUser =  new User({ username , email , password: hashedPassword });
+   
+   await newUser.save();
+
+   try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    return next(error)
+  }
+
+}
--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
-
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/user.auth.js';
+
 const app = express();
 dotenv.config();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGOOSE_URI)
 .then(() => {
@@ -16,3 +20,17 @@ mongoose.connect(process.env.MONGOOSE_URI)
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+
+    res.status(statusCode).json({ 
+        success: false,
+        message: message,
+        statusCode: statusCode
+    });
+});
