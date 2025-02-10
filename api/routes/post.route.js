7c3ea7c53cba44619ac08db2c5aac2ea0563ee03
sankarsharn <sankarsharnrastogi@gmail.com>
2025-02-11 03:36:40 +0530

    Delete post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyToken.js';
-import { create } from '../controllers/post.controller.js';
+import { create, getposts , deletePost} from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
+router.get('/getposts', getposts);
+router.delete('/deletePost/:postId/:userId', verifyToken, deletePost);
 
 export default router;
