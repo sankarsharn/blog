69b50876e9724305eb0df8dd4a2eeb84861a190b
sankarsharn <sankarsharnrastogi@gmail.com>
2025-02-13 01:00:52 +0530

    Added comment section and api routes

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,11 +2,13 @@ import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CommentSection from '../components/CommentSection';
+
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+
   useEffect(() => {
     const fetchPost = async () => {
       try {
@@ -30,24 +32,34 @@ export default function PostPage() {
     };
     fetchPost();
   }, [postSlug]);
+
   if (loading)
     return (
       <div className='flex justify-center items-center min-h-screen'>
         <Spinner size='xl' />
       </div>
     );
-  return <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
-    <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>{post && post.title}</h1>
-    <Link to={`/search?category=${post && post.category}`} className='self-center mt-5'>
-    <Button color='gray' pill size='xs'>{post && post.category}</Button>
-    </Link>
-    <img src={post && post.image} alt={post && post.title} className='mt-10 p-3 max-h-[600px] w-full object-cover'/>
-    <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
+
+  if (error) return <p className='text-center text-red-500'>Error loading post.</p>;
+
+  return (
+    <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
+      <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
+        {post && post.title}
+      </h1>
+      <Link to={`/search?category=${post?.category}`} className='self-center mt-5'>
+        <Button color='gray' pill size='xs'>
+          {post?.category}
+        </Button>
+      </Link>
+      <img src={post?.image} alt={post?.title} className='mt-10 p-3 max-h-[600px] w-full object-cover' />
+      <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className='italic'>{post && (post.content.length /1000).toFixed(0)} mins read</span>
-    </div>
-    <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
-    </div>
-    <CommentSection postId = {post._id}/>
-  </main>;
+        <span className='italic'>{post?.content?.length / 1000} mins read</span>
+      </div>
+      <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{ __html: post?.content }}></div>
+
+      {post && <CommentSection postId={post._id} />}
+    </main>
+  );
 }
